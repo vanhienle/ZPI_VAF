85c955fcfab9007226afd77478d74c8b4871e7e9
Van Hien Le <actionboyvn@gmail.com>
2023-10-03 14:09:35 +0200

    Add responsive footer

--- iaff_front/src/components/Footer/Footer.jsx
+++ iaff_front/src/components/Footer/Footer.jsx
@@ -12,86 +12,89 @@ export default function Footer() {
     <div>
       <MDBFooter className="text-center text-text bg-footerBackground border bottom-0">
         <section className="">
-          <MDBContainer className="text-center text-md-start mt-5">
-            <MDBRow className="mt-3">
-              <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  <MDBIcon color="secondary" icon="gem" className="me-3" />
-                  About project
-                </h6>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                </p>
-              </MDBCol>
+          <MDBContainer className="text-center text-md-start">
+            <MDBRow className="mt-4">
+              <div className="flex max-md:text-sm max-md:grid max-md:grid-cols-2">
+                <MDBCol md="4" lg="4" xl="3" className="mx-auto mb-2">
+                  <h6 className="text-uppercase fw-bold mb-4">About project</h6>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  </p>
+                </MDBCol>
 
-              <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Tech stack</h6>
-                <p>
-                  <a href="#!" className="text-reset">
-                    React
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Tailwind
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Python
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Langchain
-                  </a>
-                </p>
-              </MDBCol>
+                <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-2">
+                  <h6 className="text-uppercase fw-bold mb-4">Tech stack</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      React
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Tailwind
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Python
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Langchain
+                    </a>
+                  </p>
+                </MDBCol>
 
-              <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Link 1
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Link 2
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Link 3
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">
-                    Link 4
-                  </a>
-                </p>
-              </MDBCol>
+                <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-2">
+                  <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Link 1
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Link 2
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Link 3
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Link 4
+                    </a>
+                  </p>
+                </MDBCol>
 
-              <MDBCol md="4" lg="3" xl="4" className="mx-auto mb-md-0 mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-                <p>
-                  <MDBIcon color="secondary" icon="home" className="me-2" />
-                  Politechnika Wrocławska, Wrocław, Poland
-                </p>
-                <p>
-                  <MDBIcon color="secondary" icon="envelope" className="me-3" />
-                  info@student.pwr.edu.pl
-                </p>
-                <p>
-                  <MDBIcon color="secondary" icon="phone" className="me-3" /> +
-                  48 123 456 789
-                </p>
-                <p>
-                  <MDBIcon color="secondary" icon="print" className="me-3" /> +
-                  48 123 456 789
-                </p>
-              </MDBCol>
+                <MDBCol md="4" lg="3" xl="4" className="mx-auto mb-md-0 mb-2">
+                  <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p>
+                    <MDBIcon color="secondary" icon="home" className="me-2" />
+                    Politechnika Wrocławska, Wrocław, Poland
+                  </p>
+                  <p>
+                    <MDBIcon
+                      color="secondary"
+                      icon="envelope"
+                      className="me-3"
+                    />
+                    info@student.pwr.edu.pl
+                  </p>
+                  <p>
+                    <MDBIcon color="secondary" icon="phone" className="me-3" />{" "}
+                    + 48 123 456 789
+                  </p>
+                  <p>
+                    <MDBIcon color="secondary" icon="print" className="me-3" />{" "}
+                    + 48 123 456 789
+                  </p>
+                </MDBCol>
+              </div>
             </MDBRow>
           </MDBContainer>
         </section>
--- iaff_front/src/pages/Assistant/Assistant.jsx
+++ iaff_front/src/pages/Assistant/Assistant.jsx
@@ -1,23 +1,75 @@
-import React from "react";
-import { ChatList, Input } from "react-chat-elements";
+import React, { useState } from "react";
+import { MessageList, Input, Button } from "react-chat-elements";
+import { HiOutlineMenu } from "react-icons/hi";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import "./Assistant.css";
+
 const Assistant = () => {
+  const [toggleHistory, setToggleHistory] = useState(false);
+
   return (
-    <div className="chat-box">
-      <ChatList
-        className="chat-list"
-        dataSource={[
-          {
-            avatar: "https://cdn-icons-png.flaticon.com/512/4712/4712109.png",
-            alt: "assistant_avatar",
-            title: "Assistant",
-            subtitle: "How can I help you today?",
-            date: new Date(),
-            unread: 0,
-          },
-        ]}
-      />
-    </div>
+    <>
+      <div className="chat-container">
+        {toggleHistory && (
+          <div className="history-box">
+            <p className="text-center">CHAT HISTORY</p>
+          </div>
+        )}
+        {toggleHistory ? (
+          <HiOutlineMenuAlt3
+            className="text-primary-900 hover:text-primary-500"
+            size={37}
+            onClick={() => setToggleHistory(false)}
+          />
+        ) : (
+          <HiOutlineMenu
+            className="text-primary-900 hover:text-primary-500"
+            size={37}
+            onClick={() => setToggleHistory(true)}
+          />
+        )}
+
+        <div className={`chat-area-${toggleHistory ? "cut" : "full"}`}>
+          <div className="chat-box">
+            <MessageList
+              className="message-list"
+              lockable={true}
+              toBottomHeight={"100%"}
+              dataSource={[
+                {
+                  position: "left",
+                  type: "text",
+                  avatar:
+                    "https://cdn-icons-png.flaticon.com/512/4712/4712109.png",
+                  title: "Assistant",
+                  text: "How can I help you today?",
+                },
+                {
+                  position: "right",
+                  type: "text",
+                  title: "Emre",
+                  text: "Hello, I would like to know more about Poland",
+                },
+              ]}
+            />
+          </div>
+          <div className="input-box">
+            <Input
+              className="input-bar"
+              placeholder="Type here..."
+              multiline={false}
+              rightButtons={
+                <Button
+                  text={"Send"}
+                  onClick={() => alert("Sending...")}
+                  title="Send"
+                />
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
