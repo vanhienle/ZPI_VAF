fc9ffcc0eb3b7c104252cec3d1a98573e805d1ab
Luzhanskyi Ivan <42720398+Larry30475@users.noreply.github.com>
2023-10-20 15:58:30 +0200

    backend update
    update

--- iaff_back/app.py
+++ iaff_back/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_login import LoginManager
-from main import main as main_blueprint
+from flask_cors import CORS
+# from main import main as main_blueprint
 from auth import auth as auth_blueprint
 from userAccess import Access, User
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Accordingtoallknownlawsofaviationthereisnowayabeeshouldbeabletofly'
-    app.register_blueprint(main_blueprint)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    # app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint)
     access = Access()
 
@@ -17,11 +20,16 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(username):
-        result = access.getUser(username)
+        print('inside login manager')
+        print('current username: ', username)
+        result = access.getUserFromID(username)
+        print('current result from finding user')
         if not result:
-            return
+            print('cound not find user')
+            return None
         user = User()
         user.id = result[0]
+        print('found user with id: ', id)
         return user
 
     return app
@@ -29,4 +37,4 @@ def create_app():
 app= create_app()
 
 if __name__ == '__main__':
-    app.run(host='10.182.10.71', port=5000, debug=True) #Remove debug on production
+    app.run(host='0.0.0.0', port=5000, debug=True)
