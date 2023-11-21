f1f9bb03cac48098212f9d60f1983b529b7bcd14
Luzhanskyi Ivan <42720398+Larry30475@users.noreply.github.com>
2023-11-21 16:18:35 +0100

    Backend update - document adder + rest updates
    Adding document adder
    Adding translate endpoint for restAssistant
    Fixing endpoints and methods for restDocuments
    Adding new endpoint for restSurvey
    Updating excel documents with columns and weights

---
+++ iaff_back/DocumentAdder.py
@@ -0,0 +1,93 @@
+import glob
+import math
+
+import psycopg2
+import pandas as pd
+
+class Documents:
+    def __init__(self) -> None:
+        self.DBConnection, self.DBCursor = self.createDBCursor()
+
+    def createDBCursor(self):
+        conn = psycopg2.connect(host='91.195.53.69',
+                                port=5432,
+                                database="ClientDatabase",
+                                user="postgres",
+                                password="!")
+        return conn, conn.cursor()
+
+    def purgeDocument(self):
+        print('inside purge document')
+        self.DBCursor.execute("""DELETE FROM documents""")
+        self.DBConnection.commit()
+        return True
+
+    def addDocument(self, category, title, short, info, age, kids, accom, insure, study, job, live, refugee, other,
+                    documenttype, image, links):
+        print('inside add document')
+        self.DBCursor.execute(
+            """INSERT INTO documents (category, title, info, age, kids, accom, insure, study, job, live, refugee, other, "documentType", image, short, links) 
+            VALUES (%(Category)s,%(Title)s,%(Info)s,%(Age)s,%(Kids)s,%(Accom)s,%(Insure)s,%(Study)s,%(Job)s,%(Live)s,%(Refugee)s,%(Other)s,%(DocumentType)s,%(Image)s,%(Short)s,%(Links)s)""",
+            {'Category': category,
+             'Title': title,
+             'Short': short,
+             'Info': info,
+             'Age': str(age),
+             'Kids': str(kids),
+             'Accom': str(accom),
+             'Insure': str(insure),
+             'Study': str(study),
+             'Job': str(job),
+             'Live': str(live),
+             'Refugee': str(refugee),
+             'Other': str(other),
+             'DocumentType': documenttype,
+             'Image': image,
+             'Links': links})
+        self.DBConnection.commit()
+        return True
+
+class DocumentAdder:
+    def __init__(self, path):
+        self.path = path
+
+    def getAllFiles(self):
+        files = glob.glob(self.path +'/**/*.xlsx', recursive = True)
+
+        for i in range(len(files)):
+            files[i] = files[i].replace('\\','/')
+
+        return files
+
+    def insertData(self):
+        files = self.getAllFiles()
+        docs = Documents()
+        docs.purgeDocument()
+
+        for file in files:
+            print(file)
+            df = pd.read_excel(file)
+            if "category" not in df: continue
+            category = df["category"][0].capitalize()
+            if "title" not in df: continue
+            title = df["title"][0].capitalize()
+            if "info" not in df: continue
+            info = df["info"][0]
+            desc = df["short_description"][0] if "short_description" in df else ""
+            age = 24 if "age" not in df or math.isnan(df["age"][0]) else df["age"][0]
+            kids = 0 if "kids" not in df or math.isnan(df["kids"][0]) else df["kids"][0]
+            accom = 0 if "accom" not in df or math.isnan(df["accom"][0]) else df["accom"][0]
+            insure = 0 if "insure" not in df or math.isnan(df["insure"][0]) else df["insure"][0]
+            study = 0 if "study" not in df or math.isnan(df["study"][0]) else df["study"][0]
+            job = 0 if "job" not in df or math.isnan(df["job"][0]) else df["job"][0]
+            live = 0 if "live" not in df or math.isnan(df["live"][0]) else df["live"][0]
+            refugee = 0 if "refugee" not in df or math.isnan(df["refugee"][0]) else df["refugee"][0]
+            other = 0 if "other" not in df or math.isnan(df["other"][0]) else df["other"][0]
+            documentType = None if "documentType" not in df or df["documentType"].isnull else df["documentType"][0]
+            image = None
+            links = None if "useful_links" not in df else df["useful_links"][0]
+            print(links)
+            docs.addDocument(category,title,desc,info,age,kids,accom,insure,study,job,live,refugee,other,documentType,image,links)
+
+dc = DocumentAdder('C://Users/vano/Documents/GitHub/ZPI_VAF/iaff_assistant/documents')
+dc.insertData()
--- iaff_back/documents.py
+++ iaff_back/documents.py
@@ -43,20 +43,47 @@ class Documents:
 
     def getAllByName(self, name):
         print('inside get all by name')
+
         self.DBCursor.execute(
             """SELECT * 
             FROM documents 
-            WHERE position(LOWER(title) in LOWER('%(Name)s'))>0;""",
-            {'Name': name})
+            WHERE position(%(Name)s in LOWER(title))>0;""",
+            {'Name': name.lower()})
         return self.DBCursor.fetchall()
 
+
+    def addDocument(self, category, title, short, info, age, kids, accom, insure, study, job, live, refugee, other, documenttype, image, links):
+        print('inside add survey')
+        self.DBCursor.execute(
+            """INSERT INTO documents (category, title, info, age, kids, accom, insure, study, job, live, refugee, other, "documentType", image, short, links) 
+            VALUES (%(Category)s,%(Title)s,%(Info)s,%(Age)s,%(Accom)s,%(Insure)s,%(Study)s,%(Job)s,%(Live)s,%(Refugee)s,%(Other)s,%(DocumentType)s,%(Image)s,%(Short)s,%(Links)s)""",
+            {'Category': str(int(category)),
+             'Title': str(int(title)),
+             'Short': str(int(short)),
+             'Info': str(int(info)),
+             'Age': age,
+             'Kids': str(int(kids)),
+             'Accom': str(int(accom)),
+             'Insure': str(int(insure)),
+             'Study': str(int(study)),
+             'Job': str(int(job)),
+             'Live': str(int(live)),
+             'Refugee': str(int(refugee)),
+             'Other': str(int(other)),
+             'DocumentType': documenttype,
+             'Image': str(int(image)),
+             'Links': str(int(links))})
+        self.DBConnection.commit()
+        return True
 
     def getRecommendations(self, id, age, kids, baby, teen, adult, accom, insure, study, job, live, refugee, other, documenttype):
         print('inside get recommendations')
         self.DBCursor.execute(
             """SELECT title,
             info, 
-            (Kids*%(Kids)s+Accom*%(Accom)s+Insure*%(Insure)s+Study*%(Study)s+Job*%(Job)s+Live*%(Live)s+Refugee*%(Refugee)s+Other*%(Other)s) AS score
+            (Kids*%(Kids)s+Accom*%(Accom)s+Insure*%(Insure)s+Study*%(Study)s+Job*%(Job)s+Live*%(Live)s+Refugee*%(Refugee)s+Other*%(Other)s) AS score,
+            id,
+            category
             FROM documents 
             ORDER BY score DESC
             LIMIT 6
