3dc6a529da6d6afeb6cc2af5c3b56331d5e310b4
Van Hien Le <actionboyvn@gmail.com>
2023-11-23 08:52:33 +0100

    Update server.js
    Fixed routing issues in production mode

--- iaff_front/server.js
+++ iaff_front/server.js
@@ -4,6 +4,10 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
 app.listen(3000, () => {
   console.log("HTTP server running on port 3000");
 });
